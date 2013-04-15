11a41a53b1d2fcc66f398e439fa7d484485e3e12
Brian Shirai <brixen@gmail.com>
2013-04-14 17:14:49 -0700

    Created gem.

---
+++ lib/rubysl-druby.rb
@@ -0,0 +1,7 @@
+require "rubysl-druby/version"
+
+module RubySL
+  module Druby
+    # Your code goes here...
+  end
+end
---
+++ lib/rubysl-druby/version.rb
@@ -0,0 +1,5 @@
+module RubySL
+  module Druby
+    VERSION = "0.0.1"
+  end
+end
