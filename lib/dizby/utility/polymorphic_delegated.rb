f21d2075fc8ad252045522e404e2cab9572dfafe
Nathan Currier <nathan.currier@gmail.com>
2016-01-01 12:02:55 -0700

    Reimplement PolymorphicDelegated
    The list of methods for the class will not properly show delegated methods.
    Polymorphic delegated methods now retain their visibility.

--- lib/dizby/utility/polymorphic_delegated.rb
+++ lib/dizby/utility/polymorphic_delegated.rb
@@ -2,57 +2,47 @@
 require 'dizby/utility/classic_access'
 require 'dizby/utility/delegator'
 require 'dizby/utility/force_bind'
+require 'dizby/utility/method'
 
 module Dizby
   module PolymorphicDelegated
     def self.included(base)
       base.extend(ClassMethods)
-      base.__send__(:include, InstanceMethods)
 
       base.class_eval do
-        intercepted = [base, Object, Kernel, BasicObject, InstanceMethods]
+        intercepted = [base, Object, Kernel, BasicObject]
         intercepted = intercepted.map(&:instance_methods).reduce(&:-)
 
-        @__delegated_methods__ =
-          intercepted.each_with_object({}) do |name, methods|
-            methods[name.to_sym] = base.instance_method(name)
-            base.__send__(:undef_method, name)
-          end
+        intercepted.each do |name|
+          __create_delegated_method__(name)
+        end
       end
     end
 
     module ClassMethods
       include ClassicAttributeAccess
 
-      def __delegated_methods__
-        instance_variable_get(:@__delegated_methods__)
+      def __create_delegated_method__(name)
+        method = instance_method(name)
+
+        Dizby.redefine_method(self, name) do |*args, &block|
+          delegator =
+            if args.empty? || !args.first.is_a?(Delegator)
+              self
+            else
+              args.shift
+            end
+
+          bound = Dizby.force_bind(delegator, method)
+          bound.call(*args, &block)
+        end
       end
 
       def method_added(name)
         return if %i(method_missing initialize).include?(name)
-        @__delegated_methods__[name] = instance_method(name)
-        __send__(:undef_method, name)
-        nil
-      end
-    end
+        return if Dizby.caller == 'redefine_method'
 
-    module InstanceMethods
-      def __delegate__(name, delegator, *args, &block)
-        unbound = self.class.__delegated_methods__[name]
-
-        bound = Dizby.force_bind(delegator, unbound)
-
-        bound.call(*args, &block)
-      end
-
-      def method_missing(name, *args, &block)
-        args.unshift(self) if args.empty? || !args.first.is_a?(Delegator)
-
-        __delegate__ name, *args, &block
-      end
-
-      def respond_to?(sym, _priv = false)
-        super || self.class.__delegated_methods__.keys.include?(sym)
+        __create_delegated_method__(name)
       end
     end
   end
