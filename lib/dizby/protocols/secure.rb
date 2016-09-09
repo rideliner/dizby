c712e069a5467970906f6a3442c881c3b0022fc7
Nathan Currier <nathan.currier@gmail.com>
2016-09-09 01:08:27 -0600

    Remove SemibuiltObject and TunnelFactory

--- lib/dizby/protocols/secure.rb
+++ lib/dizby/protocols/secure.rb
@@ -10,7 +10,8 @@ require 'dizby/stream/connection'
 require 'dizby/server/basic'
 require 'dizby/tunnel/basic'
 require 'dizby/tunnel/basic_spawn'
-require 'dizby/tunnel/factory'
+require 'dizby/tunnel/local_strategy'
+require 'dizby/tunnel/bidirectional_strategy'
 
 require 'socket'
 
@@ -26,13 +27,15 @@ module Dizby
     ) do |args, server, (user, host, port, query)|
       port = port.to_i
 
-      factory = TunnelFactory.new(server, port)
-      tunnel = factory.create(BasicSpawnTunnel).with(user, host, args)
+      delegate = create_delegated_tunnel(
+        BasicSpawnTunnel, server, port, user, host, args
+      )
+
+      tunnel = delegate.__undelegated_get__(:@tunnel)
 
       socket = TCPSocket.open('localhost', tunnel.local_port)
       TCProtocol.apply_sockopt(socket)
 
-      delegate = delegatable_tunnel(factory, server, tunnel)
       client = BasicClient.new(
         delegate, socket, "#{scheme}://#{host}:#{tunnel.remote_port}"
       )
@@ -48,25 +51,31 @@ module Dizby
     ) do |args, server, (user, host, port, query)|
       port = port.to_i
 
-      factory = TunnelFactory.new(server, port)
-      tunnel = factory.create(BasicTunnel).with(user, host, args)
+      delegate = create_delegated_tunnel(
+        BasicTunnel, server, port, user, host, args
+      )
+
+      tunnel = delegate.__undelegated_get__(:@tunnel)
 
       socket = TCPSocket.open('localhost', tunnel.local_port)
       TCProtocol.apply_sockopt(socket)
 
-      delegate = delegatable_tunnel(factory, server, tunnel)
       client = BasicClient.new(delegate, socket, "#{scheme}://#{host}:#{port}")
       query &&= QueryRef.new(query)
 
       [client, query]
     end
 
-    def self.delegatable_tunnel(factory, server, tunnel)
-      if factory.bidirectional?
-        ResponseTunnel.new(server, tunnel)
-      else
-        DirectTunnel.new(server, tunnel)
-      end
+    def self.create_delegated_tunnel(tunnel_type, server, port, *args)
+      strategy, delegated_type =
+        if server.respond_to?(:port)
+          [BidirectionalTunnelStrategy.new(port, server.port), ResponseTunnel]
+        else
+          [LocalTunnelStrategy.new(port), DirectTunnel]
+        end
+
+      tunnel = tunnel_type.new(server, strategy, *args)
+      delegated_type.new(server, tunnel)
     end
 
     class DirectTunnel < PolyDelegate::Delegator
@@ -84,15 +93,9 @@ module Dizby
     end
 
     class ResponseTunnel < DirectTunnel
-      def initialize(server, tunnel)
-        super(server, tunnel)
-
-        @remote_port = tunnel.remote_port
-      end
-
       def uri # overload the uri of the server
         # we use the drb protocol for the rebound connection
-        "drb://localhost:#{@remote_port}"
+        "drb://localhost:#{@tunnel.remote_port}"
       end
     end
   end
